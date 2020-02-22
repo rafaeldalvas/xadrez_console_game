9e9799ac3f6018e1f9a98135927945d13e0d0045
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-02-21 23:41:01 -0300

    creat position class

---
+++ xadrez_console_game/Board/Position.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Board {
+    class Position {
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position(int line, int column) {
+            this.line = line;
+            this.column = column;
+        }
+
+        public override string ToString() {
+            return line + ", " + column;
+        }
+
+    }
+}
