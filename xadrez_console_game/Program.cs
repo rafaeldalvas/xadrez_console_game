a2bba6f7968ce09e21b0fe1d9e8024f3912258a7
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-02-24 18:42:45 -0300

    creating a ChessPosition class

--- xadrez_console_game/Program.cs
+++ xadrez_console_game/Program.cs
@@ -5,15 +5,20 @@ using chess;
 namespace xadrez_console_game {
     class Program {
         static void Main(string[] args) {
-            try {
-                Board board = new Board(8, 8);
-                board.insertPiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.insertPiece(new King(board, Color.Black), new Position(1, 3));
-                board.insertPiece(new Tower(board, Color.Black), new Position(1, 3));
-                Screen.printBoard(board);
-            } catch (BoardException e) {
-                Console.WriteLine(e.Message);
-            }
+            //try {
+            //    Board board = new Board(8, 8);
+            //    board.insertPiece(new Tower(board, Color.Black), new Position(0, 0));
+            //    board.insertPiece(new King(board, Color.Black), new Position(1, 3));
+            //    board.insertPiece(new Tower(board, Color.Black), new Position(1, 3));
+            //    Screen.printBoard(board);
+            //} catch (BoardException e) {
+            //    Console.WriteLine(e.Message);
+            //}
+
+            ChessPosition pos = new ChessPosition('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosition());
+
             Console.ReadLine();
         }
     }
