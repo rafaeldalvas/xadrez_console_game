7895d3d5a5f681d0fd57ad8a5f8aa5a1a23dd26e
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-02-24 19:02:36 -0300

    diversify colors on the console

--- xadrez_console_game/Screen.cs
+++ xadrez_console_game/Screen.cs
@@ -6,14 +6,27 @@ namespace xadrez_console_game {
     class Screen {
         public static void printBoard(Board board) {
             for (int i = 0; i < board.lines; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.columns; j++) {
                     if (board.piece(i, j) == null) {
                         Console.Write("- ");
                     } else {
-                        Console.Write(board.piece(i, j) + " ");
+                        printScreen(board.piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void printScreen(Piece piece) {
+            if (piece.color == Color.White) {
+                Console.Write(piece);
+            } else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
